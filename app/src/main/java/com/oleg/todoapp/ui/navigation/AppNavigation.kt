fa634a0f11376d15bdaf9f84ab53a8bf8bea761e
yemelinart <yemelinart@gmail.com>
2026-05-14 22:56:53 -0400

    Add Compose navigation

---
+++ app/src/main/java/com/oleg/todoapp/ui/navigation/AppNavigation.kt
@@ -0,0 +1,77 @@
+package com.oleg.todoapp.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.oleg.todoapp.TodoApplication
+import com.oleg.todoapp.ui.screens.details.TodoDetailsScreen
+import com.oleg.todoapp.ui.screens.details.TodoDetailsViewModel
+import com.oleg.todoapp.ui.screens.list.TodoListScreen
+import com.oleg.todoapp.ui.screens.list.TodoListViewModel
+
+private sealed class Destination(val route: String) {
+    data object TodoList : Destination("todos")
+    data object TodoDetails : Destination("todos/{todoId}") {
+        fun createRoute(todoId: Int): String = "todos/$todoId"
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+    val appContainer = (LocalContext.current.applicationContext as TodoApplication).appContainer
+
+    NavHost(
+        navController = navController,
+        startDestination = Destination.TodoList.route,
+    ) {
+        composable(route = Destination.TodoList.route) {
+            val viewModel: TodoListViewModel = viewModel(
+                factory = TodoListViewModel.provideFactory(appContainer.todoRepository),
+            )
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+            TodoListScreen(
+                uiState = uiState,
+                onTodoClick = { todoId ->
+                    navController.navigate(Destination.TodoDetails.createRoute(todoId))
+                },
+                onRetryClick = viewModel::loadTodos,
+            )
+        }
+
+        composable(
+            route = Destination.TodoDetails.route,
+            arguments = listOf(
+                navArgument("todoId") {
+                    type = NavType.IntType
+                },
+            ),
+        ) { backStackEntry ->
+            val todoId = checkNotNull(backStackEntry.arguments?.getInt("todoId")) {
+                "Todo id is required"
+            }
+            val viewModel: TodoDetailsViewModel = viewModel(
+                key = "todo-details-$todoId",
+                factory = TodoDetailsViewModel.provideFactory(
+                    repository = appContainer.todoRepository,
+                    todoId = todoId,
+                ),
+            )
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+            TodoDetailsScreen(
+                uiState = uiState,
+                onBackClick = navController::navigateUp,
+                onRetryClick = viewModel::loadTodo,
+            )
+        }
+    }
+}
