5a702cf794163cc43626f34953496b904d0caad1
yemelinart <yemelinart@gmail.com>
2026-05-14 23:43:20 -0400

    Modernize Android project architecture

--- app/src/main/java/com/oleg/todoapp/TodoApplication.kt
+++ app/src/main/java/com/oleg/todoapp/TodoApplication.kt
@@ -1,9 +1,7 @@
 package com.oleg.todoapp
 
 import android.app.Application
-import com.oleg.todoapp.di.AppContainer
-import com.oleg.todoapp.di.DefaultAppContainer
+import dagger.hilt.android.HiltAndroidApp
 
-class TodoApplication : Application() {
-    val appContainer: AppContainer by lazy { DefaultAppContainer() }
-}
+@HiltAndroidApp
+class TodoApplication : Application()
